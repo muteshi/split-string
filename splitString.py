1d7f1173dffa2ba4ac0fa6bd51785ab9cccd85fc
Muteshi <lumteshi@gmail.com>
2020-12-23 01:19:09 +0300

    Minor modifications

--- splitString.py
+++ splitString.py
@@ -6,51 +6,51 @@ def split_string(s):
 
     len_of_string = len(s)
 
-    total_num_zeroes = 0 #initialize sum of zeroes
+    total_num_As = 0 #initialize sum of As
 
     #compute the sum of zeroes
     for i in range(len_of_string):
-        if s[i] == '0':
-            total_num_zeroes += 1
+        if s[i] == 'a':
+            total_num_As += 1
 
     #1st edge case
-    #sum of zeros is not divisible by 3
+    #sum of As is not divisible by 3
     #Return zero
-    if total_num_zeroes % 3 != 0:
+    if total_num_As % 3 != 0:
         return 0
 
     #2nd edge case
-    #sum of zeros  is 0
+    #sum of As  is 0
     #Return zero
-    if total_num_zeroes == 0:
+    if total_num_As == 0:
         return 0
     
 
-    #initialize sum of zeroes in each string part
-    sum_of_zeroes_in_str_part = total_num_zeroes // 3
+    #initialize sum of As in each string part
+    sum_of_As_in_str_part = total_num_As // 3
 
     #initialize ways to cut string
     first_cut,second_cut = 0,0
 
     #initialize count
-    zero_count = 0
+    a_count = 0
 
     #loop from the start
     for i in range(len_of_string):
 
         #increment count if element is '0'
-        if s[i] == '0':
-            zero_count += 1
+        if s[i] == 'a':
+            a_count += 1
 
         
-        if (zero_count == sum_of_zeroes_in_str_part):
+        if (a_count == sum_of_As_in_str_part):
             first_cut += 1
 
-        elif (zero_count == 2 * sum_of_zeroes_in_str_part):
+        elif (a_count == 2 * sum_of_As_in_str_part):
             second_cut += 1
 
     return first_cut * second_cut
 
 #driver code
-print(split_string('01010'))
+print(split_string('ababa'))
 
